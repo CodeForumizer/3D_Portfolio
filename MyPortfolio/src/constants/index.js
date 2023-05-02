dcf3ebb856ac0191113dd6ba2f4d73ab50953704
Forum Gorawala <84411852+CodeForumizer@users.noreply.github.com>
2023-05-01 20:39:10 -0400

    #port ready

---
+++ MyPortfolio/src/constants/index.js
@@ -0,0 +1,253 @@
+import {
+    mobile,
+    backend,
+    creator,
+    web,
+    javascript,
+    typescript,
+    html,
+    css,
+    reactjs,
+    redux,
+    tailwind,
+    nodejs,
+    mongodb,
+    git,
+    figma,
+    docker,
+    goc,
+    carms,
+    ac,
+    mobileapp,
+    sumz,
+    jobsift,
+    threejs,
+  } from "../assets";
+  
+  export const navLinks = [
+    {
+      id: "about",
+      title: "About",
+    },
+    {
+      id: "work",
+      title: "Work",
+    },
+    {
+      id: "contact",
+      title: "Contact",
+    },
+  ];
+  
+  const services = [
+    {
+      title: "Web & Mobile App Developer",
+      icon: web,
+    },
+    {
+      title: "Full-Stack Architecture Design",
+      icon: mobile,
+    },
+    {
+      title: "API Development & Integration",
+      icon: backend,
+    },
+    {
+      title: "Testing & Debugging",
+      icon: creator,
+    },
+  ];
+  
+  const technologies = [
+    {
+      name: "HTML 5",
+      icon: html,
+    },
+    {
+      name: "CSS 3",
+      icon: css,
+    },
+    {
+      name: "JavaScript",
+      icon: javascript,
+    },
+    {
+      name: "TypeScript",
+      icon: typescript,
+    },
+    {
+      name: "React JS",
+      icon: reactjs,
+    },
+    {
+      name: "Redux Toolkit",
+      icon: redux,
+    },
+    {
+      name: "Tailwind CSS",
+      icon: tailwind,
+    },
+    {
+      name: "Node JS",
+      icon: nodejs,
+    },
+    {
+      name: "MongoDB",
+      icon: mongodb,
+    },
+    {
+      name: "Three JS",
+      icon: threejs,
+    },
+    {
+      name: "git",
+      icon: git,
+    },
+    {
+      name: "figma",
+      icon: figma,
+    },
+    {
+      name: "docker",
+      icon: docker,
+    },
+  ];
+  
+  const experiences = [
+    {
+      title: "Programmer Analyst, Application Development",
+      company_name: "Statistics Canada [ Government of Canada ]",
+      icon: goc,
+      iconBg: "#FAF5F4",
+      date: "2021-2023",
+      points: [
+        "Collaborated closely with product managers, designers, and developers to design, develop, and deploy a sophisticated mobile application, resulting in a 25% boost in user engagement. Contributed to architecture, coding, and testing efforts, ensuring alignment with project timelines and business requirements. Led the development and launch of a successful mobile application StatsCAN with over 8.9k downloads in the Apple Store and 2.1k downloads in the Google Play Store, maintaining a high average rating of 4-5 stars across both platforms. Continuously monitored and analysed user feedback and app metrics, identifying opportunities for future growth.",
+        "Conducted efforts to identify and resolve a critical bug in the mobile application , resulting in a 30% reduction in app crashes. Conducted thorough testing and root cause analysis to diagnose the issue,coordinated with the development team to devise and implement an effective solution, and monitored the app's performance post-deployment to ensure continued stability. Demonstrated strong problem-solving and collaboration skills in driving the successful resolution of the issue.",
+        "Served as a mentor to new developers in the team, sharing project knowledge and providing technical guidance and support to junior team members. Facilitated the onboarding process and helped new team members quickly get up to speed with project requirements and coding best practices.",
+        "Actively contributed to feature development efforts, including reviewing pull requests and conducting code reviews to ensure compliance with coding standards and project requirements.Demonstrated strong attention to detail and a commitment to delivering high-quality code, while fostering a collaborative and supportive team culture.",
+      ],
+    },
+    {
+      title: "Web Developer Co-op",
+      company_name: "The Canadian Resident Matching Service",
+      icon: carms,
+      iconBg: "#F6fdfd",
+      date: "2020 - 2021",
+      points: [
+        "Built a Robot Automation Framework from scratch to automate testing for two large-scale applications. This included creating an architecture incorporated best practices for test automation and developing custom libraries and test cases. The framework also reduced manual testing efforts by 80% and increased test coverage by 50%, resulting in higher quality software and improved overall testing processes.",
+        "Integrated Oracle database to the testing framework using custom-built Python methods (keywords), enhancing the framework's capabilities and improving test data management.",
+        "Successfully resolved multiple bugs and rolled out key enhancements to existing websites, resulting in significant improvements to web functionality. Examples include optimizing site speed by reducing page load times by 50%, improving user experience through intuitive navigatio and layout, and integrating new features that boosted site engagement by 20%.",
+        "Formulated and enacted report generation capabilities for all types of test runs using the Robot Framework. This involved producing custom libraries and templates to ensure consistency and clarity of the reports. Successfully generated 500+ automated reports, providing key insights for the testing team and stakeholders.",
+      ],
+    },
+    {
+      title: "Peer Tutor",
+      company_name: "Algonquin College of Applied Arts & Technology",
+      icon: ac,
+      iconBg: "white",
+      date: "2019 - 2019",
+      points: [
+        "Adapted teaching methodologies to cater to the unique learning needs of individual students,resulting in a 30% improvement in overall student performance.",
+        "Conducted one-on-one tutoring sessions for students in computer programming courses, providing support for topics including Java, Python, and data structures.",
+        "Developed and led group review sessions to promote collaborative learning and improve exam preparation, resulting in a 25% increase in exam scores for participating students.",
+        "Stayed up-to-date with the latest programming languages and technologies, attending workshops and conferences to enhance knowledge and skills.",
+      ],
+    },
+  
+  ];
+  
+  const testimonials = [
+    {
+      testimonial:
+        " She clearly understood the design decisions, client requirements and shared ideas to implement better solutions. Her work far exceeded our expectations as she writes clean code and tests well.When the Dev lead decided to take an unexpected leave, she stepped up, which helped us making informed development decisions. Her in-depth knowledge in mobile apps has helped us build a better overall product . At a personal level, Forum is self-managing, curious learner with an amazing personality.Her performance over the last year with me is an excellent example that she would be a great asset to any organization.",
+      name: "Rajat Sen",
+      designation: "Product Designer",
+      company: "Statistics Canada",
+      image: "https://media.licdn.com/dms/image/C5603AQF0K14osxxa0g/profile-displayphoto-shrink_200_200/0/1550534028547?e=1688601600&v=beta&t=wDMTOhoLxwrbDKez-q_7aDnq0U_tyow95L-Y2ias294",
+    },
+    {
+      testimonial:
+        "Forum is a great asset as a web developer and is quite talented with automation tools. She helped our team by building automation test suites for our web app. She communicates well and shows a willingness to learn.",
+      name: "Pradyumna Addala",
+      designation: "Senior Software Engineer",
+      company: "Glassdoor",
+      image: "https://media.licdn.com/dms/image/C5603AQEz8srJRceRMw/profile-displayphoto-shrink_200_200/0/1654726341944?e=1688601600&v=beta&t=Ola05nSN4L0fA_Zb6KKDtY7zVvBrM5K0ZWb5S4p6fkA",
+    },
+    {
+      testimonial:
+        "In addition to her technical skills, Forum also exhibited excellent communication and problem-solving abilities throughout the project. She took the time to listen to our needs and preferences and translated them into a visually appealing and user-friendly website. Her attention to detail and willingness to go the extra mile to ensure the project's success were greatly appreciated. We received numerous compliments on the website's design and functionality, all thanks to Forum's expertise and dedication. Overall, working with Forum was a great experience, and we highly recommend her to anyone seeking a skilled and reliable software developer.",
+      name: "Diana Marunda",
+      designation: "Staffing & Recruiter Advisor",
+      company: "Fisheries & Oceans Canada",
+      image: "https://media.licdn.com/dms/image/C5103AQGGat9h7hVS7Q/profile-displayphoto-shrink_200_200/0/1517543096344?e=1688601600&v=beta&t=MDPVzWyzwtTzYLFvjKYmpuOxELT5kz6Ktb_i77_C1Mk",
+    },
+  ];
+  
+  const projects = [
+    {
+      name: "StatsCAN",
+      description:
+        "This app provides a wealth of information on Canada's economy, society, and environment, including expert analysis, engaging visuals, intriguing short stories, and insightful articles. It's a comprehensive tool that brings together data and tools to keep you up to date with the latest developments in Canada.",
+      tags: [
+        {
+          name: "react native",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "expo",
+          color: "green-text-gradient",
+        },
+        {
+          name: "firebase",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: mobileapp,
+      source_code_link: "https://www.statcan.gc.ca/en/sc/mobile-applications?HPA=1",
+    },
+    {
+      name: "Sumz",
+      description:
+        "This application uses OpenAI's GPT model for article summarization. The application is built using ReactJS and Vite, and features a responsive and visually appealing user interface designed with Tailwind CSS. It also includes advanced RTK query API requests that fire conditionally, local storage to save history and more.",
+      tags: [
+        {
+          name: "react",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "OpenAI",
+          color: "green-text-gradient",
+        },
+        {
+          name: "Tailwind CSS",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: sumz,
+      source_code_link: "https://github.com/CodeForumizer/Sumz",
+    },
+    {
+      name: "Jobsift",
+      description:
+        "Jobsift is an ultra-fast and simple job search tool that gathers job postings from popular websites like LinkedIn, Indeed, Glassdoor, ZipRecruiter, and BeBee. Its functionalities include fetching data from an external API, implementing search and pagination features, creating custom API data fetching hooks, and ensuring clean, organized, and maintainable code with proper architecture.",
+      tags: [
+        {
+          name: "react native",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "mobile app",
+          color: "green-text-gradient",
+        },
+        {
+          name: "jobstorm",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: jobsift,
+      source_code_link: "https://github.com/CodeForumizer/Jobsift/tree/main/JobStorm",
+    },
+  ];
+  
+  export { services, technologies, experiences, testimonials, projects };
+  
